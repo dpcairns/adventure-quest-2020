82d0b7609385e5b71acb1b95cd2cbe9cd29827b9
harriet-the-spy <dpcairns@gmail.com>
2020-10-15 11:12:47 -0700

    add links to page

--- map/map.js
+++ map/map.js
@@ -1,3 +1,12 @@
 import quests from '../data.js';
 
-console.log(quests);
+const ul = document.querySelector('ul');
+
+// i want to put every quest on the page as a link
+quests.forEach(quest => {
+    const link = document.createElement('a');
+
+    link.textContent = quest.title;
+
+    ul.append(link);
+});
